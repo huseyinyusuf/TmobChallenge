15f7073e4728a940e4f753f845ab9c9096879eef
Huseyin Yusuf <huseyinyusuf1@gmail.com>
2017-04-28 22:23:05 +0300

    Front End created for Main Page Auto Layouts + Constraints included to the Code Sagues have been created

---
+++ TMob FS Challenge/MainPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TMob FS Challenge
+//
+//  Created by Huseyin Yusuf on 28/04/2017.
+//  Copyright © 2017 Huseyin Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController,UITextFieldDelegate{
+
+    @IBOutlet weak var searchButton: UIView!
+    @IBOutlet weak var areaTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchButton.layer.cornerRadius=5
+        areaTextField.delegate=self
+        locationTextField.delegate=self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "PlacesPageSague", sender: self)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        }
+
+
+}
+
